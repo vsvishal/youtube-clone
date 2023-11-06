6601359955fd2b49dc2bd218999ae0650a2ac4eb
Vishal Sharma <vsvishal11@yahoo.com>
2023-11-06 17:12:52 +0530

    exta line removed

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,11 +3,10 @@ import Sidebar from "./Sidebar";
 import { useSelector } from "react-redux";
 import Head from "./Head";
 import { Outlet } from "react-router-dom";
-import useGetChannels from "../utils/hook/useGetChannels";
 
 function Body() {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
-  // useGetChannels();
+
   return (
     <div className="font-Roboto">
       <Head />
